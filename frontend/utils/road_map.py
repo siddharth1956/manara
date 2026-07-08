e7b21e9203032e5078fc5af442dcae88093a8a68
siddharth1956 <siddharth.shetty01@sarasai.org>
2026-07-08 13:01:20 +0530

    Update road_map.py

--- frontend/utils/road_map.py
+++ frontend/utils/road_map.py
@@ -1,29 +1,14 @@
-import geopandas as gpd
-import numpy as np
+import json
 
 
 def load_road_map():
 
-    gdf = gpd.read_file(
-        "data/processed/dubai_roads.geojson"
-    )
+    with open(
+        "data/processed/dubai_roads.geojson",
+        "r",
+        encoding="utf-8"
+    ) as f:
 
-    # Convert ndarray values to strings
-    def clean(value):
+        geojson = json.load(f)
 
-        if isinstance(value, np.ndarray):
-
-            if len(value) == 0:
-                return ""
-
-            return str(value[0])
-
-        return value
-
-    for col in gdf.columns:
-
-        if col != "geometry":
-
-            gdf[col] = gdf[col].apply(clean)
-
-    return gdf
+    return geojson
